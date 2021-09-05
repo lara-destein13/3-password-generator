50ddd83bf75ae699a0941055b21087ba54aa3871
Lara Destein <laradestein13@gmail.com>
2021-09-05 14:58:05 -0700

    merge branch password-button-operates

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  // passwordText.value = password;
+  alert("The button works");
 }
 
 // Add event listener to generate button
