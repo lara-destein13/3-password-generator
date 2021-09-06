a290d1c0e470e36679afee37dc408827a7a3e212
Lara Destein <laradestein13@gmail.com>
2021-09-05 20:57:24 -0700

    merge branch characters

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var length = 0;
+var eligibleCharacters = '';
 
 var generateBtn = document.querySelector("#generate");
 
@@ -15,9 +16,94 @@ function getLength() {
   }
 }
 
+function getNumeric() {
+  while(true) {
+    var response = window.prompt("Would you like your password to include numeric characters?");
+    if (response !== null) {
+      response = response.toLowerCase();
+      if (response === 'n') {
+        return;
+      }
+      if (response === 'y') {
+        eligibleCharacters += '0123456789';
+        return;
+      }
+    }
+    alert('Please answer "Y" or "N"');
+  }
+}
+
+function getLowercase() {
+  while(true) {
+    var response = window.prompt("Would you like your password to include lowercase letters?");
+    if (response !== null) {
+      response = response.toLowerCase();
+      if (response === 'n') {
+        return;
+      }
+      if (response === 'y') {
+        eligibleCharacters += 'abcdefghijklmnopqrstuvwxyz';
+        return;
+      }
+    }
+    alert('Please answer "Y" or "N"');
+  }
+}
+
+function getUppercase() {
+  while(true) {
+    var response = window.prompt("Would you like your password to include uppercase letters?");
+    if (response !== null) {
+      response = response.toLowerCase();
+      if (response === 'n') {
+        return;
+      }
+      if (response === 'y') {
+        eligibleCharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return;
+      }
+    }
+    alert('Please answer "Y" or "N"');
+  }
+}
+
+function getSpecial() {
+  while(true) {
+    var response = window.prompt("Would you like your password to include special characters?");
+    if (response !== null) {
+      response = response.toLowerCase();
+      if (response === 'n') {
+        return;
+      }
+      if (response === 'y') {
+        eligibleCharacters += ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+        return;
+      }
+    }
+    alert('Please answer "Y" or "N"');
+  }
+}
+
+function getCharacters() {
+  while(true) {
+    eligibleCharacters = '';
+    getNumeric();
+    getLowercase();
+    getSpecial();
+    getUppercase();
+    if (eligibleCharacters.length > 0) {
+      return;
+    }
+    alert('You must allow one or more of "digits", "lowercase", "special" or "uppercase"');
+  }
+}
+
+
 function main() {
   getLength();
   alert(`length: ${length}`);
+  getCharacters();
+  alert(`eligibleCharacters: ${eligibleCharacters}`);
 }
 
 generateBtn.addEventListener("click", main);
