50919474e2d0ea1db9b33135e23b6fe79733fe03
Lara Destein <laradestein13@gmail.com>
2021-09-05 21:26:48 -0700

    fix merge conflicts

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var eligibleCharacters = '';
 var generateBtn = document.querySelector("#generate");
 var passwordArea = document.querySelector("#password");
 
+// Prompt the user for length. If length does not meet requirements then print
+// an error message and prompt the user again.
 function getLength() {
   while(true) {
     length = window.prompt("Enter password length (8 to 128)");
-    if (length !== null) {
+    if (length !== null && length !== '') {
       length = parseInt(length);
       if (length >= 8 && length <= 128) {
         return length;
@@ -17,6 +19,8 @@ function getLength() {
   }
 }
 
+// Prompt the user regarding numeric characters. If the response does not meet requirements then print
+// an error message and prompt the user again.
 function getNumeric() {
   while(true) {
     var response = window.prompt("Would you like your password to include numeric characters?");
@@ -34,6 +38,8 @@ function getNumeric() {
   }
 }
 
+// Prompt the user regarding lowercase characters. If the response does not meet requirements then print
+// an error message and prompt the user again.
 function getLowercase() {
   while(true) {
     var response = window.prompt("Would you like your password to include lowercase letters?");
@@ -51,6 +57,8 @@ function getLowercase() {
   }
 }
 
+// Prompt the user regarding uppercase characters. If the response does not meet requirements then print
+// an error message and prompt the user again.
 function getUppercase() {
   while(true) {
     var response = window.prompt("Would you like your password to include uppercase letters?");
@@ -68,6 +76,8 @@ function getUppercase() {
   }
 }
 
+// Prompt the user regarding special characters. If the response does not meet requirements then print
+// an error message and prompt the user again.
 function getSpecial() {
   while(true) {
     var response = window.prompt("Would you like your password to include special characters?");
@@ -77,7 +87,7 @@ function getSpecial() {
         return;
       }
       if (response === 'y') {
-        eligibleCharacters += ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+        eligibleCharacters += '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
         return;
       }
     }
@@ -85,6 +95,8 @@ function getSpecial() {
   }
 }
 
+// Prompt the user regarding character types. If the user has disallowed all character types
+// print an error message and prompt the user again.
 function getCharacters() {
   while(true) {
     eligibleCharacters = '';
@@ -99,12 +111,14 @@ function getCharacters() {
   }
 }
 
+// Prompt user for length and character type requirements. Generate the password by randomly 
+// selecting from the eligible characters. 
 function main() {
   getLength();
   getCharacters();
   var password = '';
   for (let i = 0; i < length; i += 1) {
-    var index = Math.floor(Math.random() * eligibleCharacters.length) + 1
+    var index = Math.floor(Math.random() * eligibleCharacters.length)
     var char = eligibleCharacters[index];
     password += char;
   }
